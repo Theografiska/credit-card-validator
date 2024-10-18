748d1ea031cbb8fa20e6abd9fdf28c0448fa7b3e
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-10-18 23:48:18 +0300

    Added more card company validation settings

--- main.js
+++ main.js
@@ -1,33 +1,3 @@
-// All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
-
-// All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
-
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4] // prints false
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9]
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3] // prints false
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3] // prints false
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3]
-
-const test1 = [7, 0, 0, 3, 5, 0, 0, 0, 6, 3, 0, 7, 2, 5, 2, 3] // an invalid card by an "unknown company"
-
-// An array of all the arrays above
-const batch = [
-    valid1, valid2, valid3, valid4, valid5, 
-    invalid1, invalid2, invalid3, invalid4, invalid5, 
-    mystery1, mystery2, mystery3, mystery4, mystery5, 
-    test1
-]
 
 
 // function to validate a credit card:
@@ -49,35 +19,13 @@ const validateCred = array => {
         mutatedArray.push(digit);
         sumCheck += digit;
     };
-    // console.log(mutatedArray); 
-    // console.log(sumCheck);
+
     if (sumCheck % 10 === 0) {
         return true;
     } else {
         return false;
     }
 }
-
-/* testing all the credit cards: 
-
-console.log(validateCred(valid1));
-console.log(validateCred(valid2));
-console.log(validateCred(valid3));
-console.log(validateCred(valid4));
-console.log(validateCred(valid5));
-
-console.log(validateCred(invalid1));
-console.log(validateCred(invalid2));
-console.log(validateCred(invalid3));
-console.log(validateCred(invalid4));
-console.log(validateCred(invalid5));
-
-console.log(validateCred(mystery1));
-console.log(validateCred(mystery2));
-console.log(validateCred(mystery3));
-console.log(validateCred(mystery4));
-console.log(validateCred(mystery5));
-*/
 
 // function to iterate through a nested array of credit cards numbers, and return all the invalid cards. 
 const findInvalidCards = nestedArr => {
@@ -91,37 +39,112 @@ const findInvalidCards = nestedArr => {
     return invalidCards;
 }
 
-// testing the previous function
-let fakeCards = findInvalidCards(batch);
-console.log(fakeCards);
-
 // function to identify the credit card companies that have issued faulty numbers:
 const idInvalidCardCompanies = invalidArr => {
     let invalidCardCompanies = [];
+
     for (card of invalidArr) {
-        if (card[0] === 3) {
+        if ((card[0] === 3 && card[1] === 4) || (card[0] === 3 && card[1] === 7)) { // Amex begins with 34 or 37
             if (invalidCardCompanies.findIndex(invalidCard => { // this is needed to make sure there are no duplicates
                 return invalidCard === 'Amex (American Express)';
             }) === -1) {
                 invalidCardCompanies.push('Amex (American Express)');
             }
-        } else if (card[0] === 4) {
+        } else if (card[0] === 3 && card[1] === 5 ){  // JCB range is actually 3528–3589
+            if (invalidCardCompanies.findIndex(invalidCard => {  
+                return invalidCard === 'JCB';
+            }) === -1) {
+                invalidCardCompanies.push('JCB');
+            }
+        } else if ((card[0] === 3 && card[1] === 0 && card[2] === 0) || // Diners Club – Carte Blanche begins with 300, 301, 302, 303, 304, 305
+                   (card[0] === 3 && card[1] === 0 && card[2] === 1) || 
+                   (card[0] === 3 && card[1] === 0 && card[2] === 2) || 
+                   (card[0] === 3 && card[1] === 0 && card[2] === 3) || 
+                   (card[0] === 3 && card[1] === 0 && card[2] === 4) || 
+                   (card[0] === 3 && card[1] === 0 && card[2] === 5)) { 
+            if (invalidCardCompanies.findIndex(invalidCard => {  
+                return invalidCard === 'Diners Club – Carte Blanche';
+            }) === -1) {
+                invalidCardCompanies.push('Diners Club – Carte Blanche');
+            }
+        } else if (card[0] === 3 && card[1] === 6) { // Diners Club – International begins with 36
+            if (invalidCardCompanies.findIndex(invalidCard => {  
+                return invalidCard === 'Diners Club – International';
+            }) === -1) {
+                invalidCardCompanies.push('Diners Club – International');
+            }
+        } else if (card[0] === 5 && card[1] === 4) { // Diners Club – USA & Canada begins with 54
+            if (invalidCardCompanies.findIndex(invalidCard => {  
+                return invalidCard === 'Diners Club – USA & Canada';
+            }) === -1) {
+                invalidCardCompanies.push('Diners Club – USA & Canada');
+            }
+        } else if ((card[0] === 4 && card[1] === 0 && card[2] === 3 && card[3] === 6) || // Visa Electron begins with 4026, 417500, 4508, 4844, 4913, 4917
+                   (card[0] === 4 && card[1] === 1 && card[2] === 7 && card[4] === 5 && card[5] === 0 && card[6] === 0) || 
+                   (card[0] === 4 && card[1] === 5 && card[2] === 0 && card[3] === 8) || 
+                   (card[0] === 4 && card[1] === 8 && card[2] === 4 && card[3] === 4) || 
+                   (card[0] === 4 && card[1] === 9 && card[2] === 1 && card[3] === 3) || 
+                   (card[0] === 4 && card[1] === 9 && card[2] === 1 && card[3] === 7)){ 
+            if (invalidCardCompanies.findIndex(invalidCard => {  
+                return invalidCard === 'Visa Electron';
+            }) === -1) {
+                invalidCardCompanies.push('Visa Electron');
+            }
+        } else if (card[0] === 4) { // Visa begins with 4
             if (invalidCardCompanies.findIndex(invalidCard => {
                 return invalidCard === 'Visa';
             }) === -1) {
                 invalidCardCompanies.push('Visa');
             }
-        } else if (card[0] === 5) {
+        } else if ((card[0] === 5 && card[1] === 1) || // Mastercard begins with 51, 52, 53, 54, 55, 222100-272099
+                   (card[0] === 5 && card[1] === 2) ||
+                   (card[0] === 5 && card[1] === 3) ||
+                   (card[0] === 5 && card[1] === 4) ||
+                   (card[0] === 5 && card[1] === 5) ||
+                   (card[0] === 2 && card[1] === 2) || // actually it's a range from 222100-272099
+                   (card[0] === 2 && card[1] === 3) ||
+                   (card[0] === 2 && card[1] === 4) ||
+                   (card[0] === 2 && card[1] === 5) ||
+                   (card[0] === 2 && card[1] === 6) ||
+                   (card[0] === 2 && card[1] === 7)){
             if (invalidCardCompanies.findIndex(invalidCard => {
                 return invalidCard === 'Mastercard';
             }) === -1) {
                 invalidCardCompanies.push('Mastercard');
             }
-        } else if (card[0] === 6) {
+        } else if ((card[0] === 5 && card[1] === 0 && card[2] === 1 && card[3] === 8) || // Maestro begins with 5018, 5020, 5038, 5893, 6304, 6759, 6761, 6762, 6763
+                   (card[0] === 5 && card[1] === 0 && card[2] === 2 && card[3] === 0) || 
+                   (card[0] === 5 && card[1] === 0 && card[2] === 3 && card[3] === 8) || 
+                   (card[0] === 5 && card[1] === 8 && card[2] === 9 && card[3] === 3) || 
+                   (card[0] === 6 && card[1] === 3 && card[2] === 0 && card[3] === 4) || 
+                   (card[0] === 6 && card[1] === 7 && card[2] === 5 && card[3] === 9) || 
+                   (card[0] === 6 && card[1] === 7 && card[2] === 6 && card[3] === 1) || 
+                   (card[0] === 6 && card[1] === 7 && card[2] === 6 && card[3] === 2) || 
+                   (card[0] === 6 && card[1] === 7 && card[2] === 6 && card[3] === 3)){
+            if (invalidCardCompanies.findIndex(invalidCard => {
+                return invalidCard === 'Maestro';
+            }) === -1) {
+                invalidCardCompanies.push('Maestro');
+            }
+        } else if ((card[0] === 6 && card[1] === 0 && card[2] === 1 && card[3] === 1) || // Discover begins with 6011, 622126-622925, 644, 645, 647, 648, 649, 65
+                   (card[0] === 6 && card[1] === 4 && card[2] === 4) || // actually it's a range: 622126-622925
+                   (card[0] === 6 && card[1] === 4 && card[2] === 5) ||
+                   (card[0] === 6 && card[1] === 4 && card[2] === 7) ||
+                   (card[0] === 6 && card[1] === 4 && card[2] === 8) ||
+                   (card[0] === 6 && card[1] === 4 && card[2] === 9) ||
+                   (card[0] === 6 && card[1] === 5)){
             if (invalidCardCompanies.findIndex(invalidCard => {
                 return invalidCard === 'Discover';
             }) === -1) {
                 invalidCardCompanies.push('Discover');
+            }
+        }  else if ((card[0] === 6 && card[1] === 3 && card[2] === 7) || // InstaPayment begins with 637, 638, 639
+                    (card[0] === 6 && card[1] === 3 && card[2] === 8) || 
+                    (card[0] === 6 && card[1] === 3 && card[2] === 9)){
+            if (invalidCardCompanies.findIndex(invalidCard => {
+                return invalidCard === 'InstaPayment';
+            }) === -1) {
+                invalidCardCompanies.push('InstaPayment');
             }
         } else {
             if (invalidCardCompanies.findIndex(invalidCard => {
@@ -134,7 +157,67 @@ const idInvalidCardCompanies = invalidArr => {
     return invalidCardCompanies;
 }
 
-// testing the previous function:
-let fakeCardCompanies = idInvalidCardCompanies(fakeCards);
-console.log(fakeCardCompanies);
+// testing cards from this site: https://www.freeformatter.com/credit-card-number-generator-validator.html
 
+
+//JCB: begins with 3528-3589
+const jcb1 = [3, 5, 4, 2, 5, 1, 0, 7, 7, 9, 8, 8, 9, 4, 8, 5]
+const jcb2 = [3, 5, 2, 8, 1, 0, 0, 6, 5, 7, 1, 5, 0, 4, 1, 8]
+const jcb3 = [3, 5, 3, 9, 6, 7, 2, 4, 0, 4, 5, 0, 3, 6, 1, 8, 4, 8, 5]
+const jcb4 = [3, 5, 5, 4]; 
+for (let i=0; i < 14 ; i++) { // generating random card numbers
+    jcb4.push(Math.floor(Math.random() * 10))
+};
+
+// Diners Club - North America: begins with 54
+const dcNa1 = [5, 4, 4, 6, 6, 2, 8, 0, 5, 8, 7, 7, 8, 7, 5, 2]
+const dcNa2 = [5, 4, 9, 7, 5, 3, 3, 5, 0, 7, 2, 4, 3, 7, 1]
+// Diners Club - Carte Blanche: begins with 300-305
+const dcCb1 = [3, 0, 3, 1, 9, 2, 7, 9, 4, 8, 8, 0, 0, 9]
+const dcCb2 = [3, 0, 2, 2, 8, 5, 1, 8, 2, 1, 3, 6, 4, 1]
+const dcCb3 = [3, 0, 3, 7, 5, 0, 0, 8, 9, 2, 6, 2, 6, 8]
+// Diners Club - International: begins with 36
+const dcI1 = [3, 6, 8, 9, 6, 4, 8, 9, 9, 4, 7, 9, 9, 3]
+const dcI2 = [3, 6, 3, 9, 3, 0, 9, 6, 2, 5, 7, 4, 4, 6]
+const dcI3 = [3, 6, 0, 4, 7, 0, 0, 0, 7, 7, 5, 4, 5]
+// Maestro: begins with 5018, 5020, 5038, 5893, 6304, 6759, 6761, 6762, 6763
+const mae1 = [6, 7, 6, 2, 5, 8, 1, 2, 6, 8, 2, 0, 3, 7, 4, 6]
+const mae2 = [6, 7, 6, 3, 2, 6, 0, 0, 7, 5, 0, 3, 1, 1, 1, 8]
+const mae3 = [5, 0, 3, 8, 8, 0, 4, 1, 4, 4, 9, 9, 3, 8, 1, 3]
+// Visa Electron: begins with 4026, 417500, 4508, 4844, 4913, 4917
+const visaEl1 = [4, 0, 4, 4, 8, 6, 4, 0, 7, 6, 9, 3, 8, 0, 0, 1]
+const visaEl2 = [4, 8, 4, 4, 2, 0, 4, 0, 6, 2, 9, 2, 5, 8, 7, 0]
+const visaEl3 = [4, 9, 1, 3, 8, 7, 4, 7, 3, 9, 1, 0, 4, 9, 3, 3]
+// InstaPayment: begins with 637, 638, 639	
+const instaPay1 = [6, 3, 7, 3, 7, 6, 5, 0, 7, 6, 0, 3, 8, 1, 0, 2]
+const instaPay2 = [6, 3, 9, 4, 7, 9, 4, 5, 4, 7, 8, 4, 2, 7, 6, 6]
+const instaPay3 = [6, 3, 8, 8, 7, 5, 3, 4, 4, 8, 5, 8, 6, 1, 0, 0]
+// Discover : begins with 6011, 622126 to 622925, 644, 645, 646, 647, 648, 649, 65
+const discover1 = [6, 0, 1, 1, 2, 4, 3, 0, 2, 6, 1, 0, 0, 1, 4, 2]
+const discover2 = [6, 4, 4, 5, 1, 5, 2, 1, 7, 5, 5, 4, 8, 0, 2, 2]
+const discover3 = [6, 4, 7, 1, 0, 7, 1, 0, 0, 6, 9, 0, 8, 1, 0, 0]
+// American Express: begins with 34, 37:
+const amex1 = [3, 4, 6, 7, 8, 1, 1, 7, 5, 7, 1, 6, 3, 4, 7]
+const amex2 = [3, 7, 2, 1, 4, 7, 1, 8, 2, 1, 7, 6, 0, 5, 0]
+// MasterCard: begins with 51, 52, 53, 54, 55, 222100-272099:
+const masterC1 = [2, 7, 2, 0, 9, 9, 4, 0, 0, 4, 0, 5, 8, 5, 0, 6]
+const masterC2 = [5, 4, 8, 4, 9, 3, 9, 4, 2, 3, 2, 8, 2, 2, 2, 0]
+
+const newBatch = [
+    jcb1, jcb2, jcb3, jcb4,
+    dcNa1, dcNa2, 
+    dcCb1, dcCb2, dcCb3,
+    dcI1, dcI2, dcI3,
+    mae1, mae2, mae3,
+    visaEl1, visaEl2, visaEl3,
+    instaPay1, instaPay2, instaPay3,
+    discover1, discover2, discover3,
+    amex1, amex2,
+    masterC1, masterC2
+]
+
+let fakeNewCards = findInvalidCards(newBatch);
+console.log(fakeNewCards);
+
+let newFakeCardCompanies = idInvalidCardCompanies(fakeNewCards);
+console.log(newFakeCardCompanies);

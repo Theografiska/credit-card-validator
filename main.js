14a3fcb3dc617f57fef48b772be7e55f8c728d82
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-10-19 11:51:44 +0300

    Added functions to turn a string into an array of numbers and to turn an invalid card to valid. Also jotted down ideas at the end of main.js on how to turn this program interactive.

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
+// All valid credit card numbers
+const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
+const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
+const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
+const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
+const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
 
+// All invalid credit card numbers
+const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
+const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
+const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
+const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
+const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
+
+// Can be either valid or invalid
+const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
+const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
+const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
+const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
+const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
+
+// An array of all the arrays above
+const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
 
 // function to validate a credit card:
 const validateCred = array => {
     let arrayCopy = array.slice(); // making a copy in order not to mutate the original array
     arrayCopy = arrayCopy.toReversed(); // reversing the array to access from the left side 
 
-    let mutatedArray = []; // this array will contain the digits which have been operated on
+    // let mutatedArray = []; // this array will contain the digits which have been operated on
     let sumCheck = 0; // this is needed to check whether the sum modulo 10 is 0
 
     for (let i = 0; i < arrayCopy.length; i++) {
@@ -16,7 +38,7 @@ const validateCred = array => {
         if (digit > 9) {
             digit -= 9;
         };
-        mutatedArray.push(digit);
+        // mutatedArray.push(digit);
         sumCheck += digit;
     };
 
@@ -157,8 +179,108 @@ const idInvalidCardCompanies = invalidArr => {
     return invalidCardCompanies;
 }
 
-// testing cards from this site: https://www.freeformatter.com/credit-card-number-generator-validator.html
+// function to create an array from a string of number:
 
+const arrayFromString = string => {
+    let stringArr = string.split('');
+
+    let numberArr = stringArr.map(string => {
+        return parseInt(string);
+    });
+    return numberArr;
+}
+
+/* Testing turning a string of a long number into an array of numbers: 
+let testString1 = '4556913208160220';
+let testStringArray = arrayFromString(testString1);
+
+console.log(testStringArray);
+console.log(validateCred(testStringArray));
+console.log(turnInvalidCardValid(testStringArray));
+*/
+
+// function to turn invalid cards to valid. By adding a number, which makes the sumCheck % 10 = 0. 
+
+const turnInvalidCardValid = cardArr => {
+    if (validateCred(cardArr) === false) {
+        let arrayCopy = cardArr.slice(); // making a copy in order not to mutate the original array
+        arrayCopy = arrayCopy.toReversed(); // reversing the array to access from the left side 
+
+        let sumCheck = 0;
+        for (let i = 0; i < arrayCopy.length; i++) {
+            let digit = arrayCopy[i];
+            if (i % 2 === 1) {
+                digit *= 2;
+            };
+            if (digit > 9) {
+                digit -= 9;
+            };
+            sumCheck += digit;
+        };
+        let remainder = sumCheck % 10;
+        if (arrayCopy[0] >= remainder) {
+            arrayCopy[0] -= remainder;
+        } else if (arrayCopy[0] < remainder) {
+            arrayCopy[0] += (10-remainder);
+        }
+
+        arrayCopy = arrayCopy.toReversed(); // reversing the array again
+
+        if (validateCred(arrayCopy) === true) {
+            return arrayCopy;
+        }
+    } else { // if a card is valid, it won't be changed
+        return cardArr;
+    }
+}
+
+/* testing turning invalid cards to valid: 
+let invalid1ToValid = turnInvalidCardValid(invalid1);
+let invalid2ToValid = turnInvalidCardValid(invalid2);
+let invalid3ToValid = turnInvalidCardValid(invalid3);
+let invalid4ToValid = turnInvalidCardValid(invalid4);
+let invalid5ToValid = turnInvalidCardValid(invalid5);
+
+console.log(invalid1ToValid);
+console.log(invalid2ToValid);
+console.log(invalid3ToValid);
+console.log(invalid4ToValid);
+console.log(invalid5ToValid);
+
+console.log(validateCred(invalid1ToValid));
+console.log(validateCred(invalid2ToValid));
+console.log(validateCred(invalid3ToValid));
+console.log(validateCred(invalid4ToValid));
+console.log(validateCred(invalid5ToValid));
+*/ 
+
+/* testing turning mystery cards to valid, without mutating correct cards:
+console.log(validateCred(mystery1));
+console.log(validateCred(mystery2));
+console.log(validateCred(mystery3));
+console.log(validateCred(mystery4));
+console.log(validateCred(mystery5));
+
+let mystery1ToValid = turnInvalidCardValid(mystery1);
+let mystery2ToValid = turnInvalidCardValid(mystery2);
+let mystery3ToValid = turnInvalidCardValid(mystery3);
+let mystery4ToValid = turnInvalidCardValid(mystery4);
+let mystery5ToValid = turnInvalidCardValid(mystery5);
+
+console.log(mystery1ToValid);
+console.log(mystery2ToValid);
+console.log(mystery3ToValid);
+console.log(mystery4ToValid);
+console.log(mystery5ToValid);
+
+console.log(validateCred(mystery1ToValid));
+console.log(validateCred(mystery2ToValid));
+console.log(validateCred(mystery3ToValid));
+console.log(validateCred(mystery4ToValid));
+console.log(validateCred(mystery5ToValid));
+*/
+
+// testing cards from this site: https://www.freeformatter.com/credit-card-number-generator-validator.html
 
 //JCB: begins with 3528-3589
 const jcb1 = [3, 5, 4, 2, 5, 1, 0, 7, 7, 9, 8, 8, 9, 4, 8, 5]
@@ -216,8 +338,117 @@ const newBatch = [
     masterC1, masterC2
 ]
 
+// tests
+/*
 let fakeNewCards = findInvalidCards(newBatch);
 console.log(fakeNewCards);
 
 let newFakeCardCompanies = idInvalidCardCompanies(fakeNewCards);
 console.log(newFakeCardCompanies);
+*/
+
+
+
+
+
+/* order of creating a website flow: 
+
+Flow A: user inserts a card number: a string
+1. function to create an array from a string of number:
+
+const arrayFromString = string => {
+    let stringArr = string.split('');
+
+    let numberArr = stringArr.map(string => {
+        return parseInt(string);
+    });
+    return numberArr;
+}
+
+2. Validate that card: 
+
+const validateCred = array => {
+    let arrayCopy = array.slice(); // making a copy in order not to mutate the original array
+    arrayCopy = arrayCopy.toReversed(); // reversing the array to access from the left side 
+
+    // let mutatedArray = []; // this array will contain the digits which have been operated on
+    let sumCheck = 0; // this is needed to check whether the sum modulo 10 is 0
+
+    for (let i = 0; i < arrayCopy.length; i++) {
+        let digit = arrayCopy[i];
+        if (i % 2 === 1) {
+            digit *= 2;
+        };
+        if (digit > 9) {
+            digit -= 9;
+        };
+        // mutatedArray.push(digit);
+        sumCheck += digit;
+    };
+
+    if (sumCheck % 10 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+3. Turn that card into a valid card:
+
+const turnInvalidCardValid = cardArr => {
+    if (validateCred(cardArr) === false) {
+        let arrayCopy = cardArr.slice(); // making a copy in order not to mutate the original array
+        arrayCopy = arrayCopy.toReversed(); // reversing the array to access from the left side 
+
+        let sumCheck = 0;
+        for (let i = 0; i < arrayCopy.length; i++) {
+            let digit = arrayCopy[i];
+            if (i % 2 === 1) {
+                digit *= 2;
+            };
+            if (digit > 9) {
+                digit -= 9;
+            };
+            sumCheck += digit;
+        };
+        let remainder = sumCheck % 10;
+        if (arrayCopy[0] >= remainder) {
+            arrayCopy[0] -= remainder;
+        } else if (arrayCopy[0] < remainder) {
+            arrayCopy[0] += (10-remainder);
+        }
+
+        arrayCopy = arrayCopy.toReversed(); // reversing the array again
+
+        if (validateCred(arrayCopy) === true) {
+            return arrayCopy;
+        }
+    } else { // if a card is valid, it won't be changed
+        return cardArr;
+    }
+}
+
+
+Flow B: user chooses to generate a credit card number of a certain company
+
+Click on a particular card company logo: 
+
+Generates a card number: 
+
+Click on a button to validate the card.
+
+Turns the card string to array. 
+
+Validates the card. 
+
+Tells the user.
+
+If invalid, click on a button to turn the card valid. 
+
+Turns invalid card array to valid.
+
+Returns the valid string. 
+
+(On top of a correctly formatted card)
+
+*/

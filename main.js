cf91d263a7c758a6250d9aca9c97c8ee2512751d
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-10-24 22:13:19 +0300

    Added the online interactivity

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8];
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9];
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6];
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5];
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6];
-
-// All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5];
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3];
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4];
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5];
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4];
-
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4];
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9];
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3];
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3];
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3];
-
-// An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5];
-
 // function to validate a credit card:
 const validateCred = array => {
     let arrayCopy = array.slice(); // making a copy in order not to mutate the original array
@@ -49,141 +25,11 @@ const validateCred = array => {
     }
 }
 
-// function to iterate through a nested array of credit cards numbers, and return all the invalid cards. 
-const findInvalidCards = nestedArr => {
-    let invalidCards = [];
-    for (let i = 0; i < nestedArr.length; i++) {
-        let card = nestedArr[i];
-        if (validateCred(card) === false) {
-            invalidCards.push(card);
-        }
-    }
-    return invalidCards;
-}
-
-// function to identify the credit card companies that have issued faulty numbers:
-const idInvalidCardCompanies = invalidArr => {
-    let invalidCardCompanies = [];
-
-    for (card of invalidArr) {
-        if ((card[0] === 3 && card[1] === 4) || (card[0] === 3 && card[1] === 7)) { // Amex begins with 34 or 37
-            if (invalidCardCompanies.findIndex(invalidCard => { // this is needed to make sure there are no duplicates
-                return invalidCard === 'Amex (American Express)';
-            }) === -1) {
-                invalidCardCompanies.push('Amex (American Express)');
-            }
-        } else if (card[0] === 3 && card[1] === 5 ){  // JCB range is actually 3528–3589
-            if (invalidCardCompanies.findIndex(invalidCard => {  
-                return invalidCard === 'JCB';
-            }) === -1) {
-                invalidCardCompanies.push('JCB');
-            }
-        } else if ((card[0] === 3 && card[1] === 0 && card[2] === 0) || // Diners Club – Carte Blanche begins with 300, 301, 302, 303, 304, 305
-                   (card[0] === 3 && card[1] === 0 && card[2] === 1) || 
-                   (card[0] === 3 && card[1] === 0 && card[2] === 2) || 
-                   (card[0] === 3 && card[1] === 0 && card[2] === 3) || 
-                   (card[0] === 3 && card[1] === 0 && card[2] === 4) || 
-                   (card[0] === 3 && card[1] === 0 && card[2] === 5)) { 
-            if (invalidCardCompanies.findIndex(invalidCard => {  
-                return invalidCard === 'Diners Club – Carte Blanche';
-            }) === -1) {
-                invalidCardCompanies.push('Diners Club – Carte Blanche');
-            }
-        } else if (card[0] === 3 && card[1] === 6) { // Diners Club – International begins with 36
-            if (invalidCardCompanies.findIndex(invalidCard => {  
-                return invalidCard === 'Diners Club – International';
-            }) === -1) {
-                invalidCardCompanies.push('Diners Club – International');
-            }
-        } else if (card[0] === 5 && card[1] === 4) { // Diners Club – USA & Canada begins with 54
-            if (invalidCardCompanies.findIndex(invalidCard => {  
-                return invalidCard === 'Diners Club – USA & Canada';
-            }) === -1) {
-                invalidCardCompanies.push('Diners Club – USA & Canada');
-            }
-        } else if ((card[0] === 4 && card[1] === 0 && card[2] === 3 && card[3] === 6) || // Visa Electron begins with 4026, 417500, 4508, 4844, 4913, 4917
-                   (card[0] === 4 && card[1] === 1 && card[2] === 7 && card[4] === 5 && card[5] === 0 && card[6] === 0) || 
-                   (card[0] === 4 && card[1] === 5 && card[2] === 0 && card[3] === 8) || 
-                   (card[0] === 4 && card[1] === 8 && card[2] === 4 && card[3] === 4) || 
-                   (card[0] === 4 && card[1] === 9 && card[2] === 1 && card[3] === 3) || 
-                   (card[0] === 4 && card[1] === 9 && card[2] === 1 && card[3] === 7)){ 
-            if (invalidCardCompanies.findIndex(invalidCard => {  
-                return invalidCard === 'Visa Electron';
-            }) === -1) {
-                invalidCardCompanies.push('Visa Electron');
-            }
-        } else if (card[0] === 4) { // Visa begins with 4
-            if (invalidCardCompanies.findIndex(invalidCard => {
-                return invalidCard === 'Visa';
-            }) === -1) {
-                invalidCardCompanies.push('Visa');
-            }
-        } else if ((card[0] === 5 && card[1] === 1) || // Mastercard begins with 51, 52, 53, 54, 55, 222100-272099
-                   (card[0] === 5 && card[1] === 2) ||
-                   (card[0] === 5 && card[1] === 3) ||
-                   (card[0] === 5 && card[1] === 4) ||
-                   (card[0] === 5 && card[1] === 5) ||
-                   (card[0] === 2 && card[1] === 2) || // actually it's a range from 222100-272099
-                   (card[0] === 2 && card[1] === 3) ||
-                   (card[0] === 2 && card[1] === 4) ||
-                   (card[0] === 2 && card[1] === 5) ||
-                   (card[0] === 2 && card[1] === 6) ||
-                   (card[0] === 2 && card[1] === 7)){
-            if (invalidCardCompanies.findIndex(invalidCard => {
-                return invalidCard === 'Mastercard';
-            }) === -1) {
-                invalidCardCompanies.push('Mastercard');
-            }
-        } else if ((card[0] === 5 && card[1] === 0 && card[2] === 1 && card[3] === 8) || // Maestro begins with 5018, 5020, 5038, 5893, 6304, 6759, 6761, 6762, 6763
-                   (card[0] === 5 && card[1] === 0 && card[2] === 2 && card[3] === 0) || 
-                   (card[0] === 5 && card[1] === 0 && card[2] === 3 && card[3] === 8) || 
-                   (card[0] === 5 && card[1] === 8 && card[2] === 9 && card[3] === 3) || 
-                   (card[0] === 6 && card[1] === 3 && card[2] === 0 && card[3] === 4) || 
-                   (card[0] === 6 && card[1] === 7 && card[2] === 5 && card[3] === 9) || 
-                   (card[0] === 6 && card[1] === 7 && card[2] === 6 && card[3] === 1) || 
-                   (card[0] === 6 && card[1] === 7 && card[2] === 6 && card[3] === 2) || 
-                   (card[0] === 6 && card[1] === 7 && card[2] === 6 && card[3] === 3)){
-            if (invalidCardCompanies.findIndex(invalidCard => {
-                return invalidCard === 'Maestro';
-            }) === -1) {
-                invalidCardCompanies.push('Maestro');
-            }
-        } else if ((card[0] === 6 && card[1] === 0 && card[2] === 1 && card[3] === 1) || // Discover begins with 6011, 622126-622925, 644, 645, 647, 648, 649, 65
-                   (card[0] === 6 && card[1] === 4 && card[2] === 4) || // actually it's a range: 622126-622925
-                   (card[0] === 6 && card[1] === 4 && card[2] === 5) ||
-                   (card[0] === 6 && card[1] === 4 && card[2] === 7) ||
-                   (card[0] === 6 && card[1] === 4 && card[2] === 8) ||
-                   (card[0] === 6 && card[1] === 4 && card[2] === 9) ||
-                   (card[0] === 6 && card[1] === 5)){
-            if (invalidCardCompanies.findIndex(invalidCard => {
-                return invalidCard === 'Discover';
-            }) === -1) {
-                invalidCardCompanies.push('Discover');
-            }
-        }  else if ((card[0] === 6 && card[1] === 3 && card[2] === 7) || // InstaPayment begins with 637, 638, 639
-                    (card[0] === 6 && card[1] === 3 && card[2] === 8) || 
-                    (card[0] === 6 && card[1] === 3 && card[2] === 9)){
-            if (invalidCardCompanies.findIndex(invalidCard => {
-                return invalidCard === 'InstaPayment';
-            }) === -1) {
-                invalidCardCompanies.push('InstaPayment');
-            }
-        } else {
-            if (invalidCardCompanies.findIndex(invalidCard => {
-                return invalidCard === 'Unknown company';
-            }) === -1) {
-                invalidCardCompanies.push('Unknown company');
-            }
-        }
-    }  
-    return invalidCardCompanies;
-}
-
-// function to just do get the card company for a single card:
+// function to get the card company for a single card:
 
 const getCardCompany = card => {
     if ((card[0] === 3 && card[1] === 4) || (card[0] === 3 && card[1] === 7)) { // Amex begins with 34 or 37
-        return'Amex (American Express)';
+        return'American Express';
     } else if (card[0] === 3 && card[1] === 5 ){  // JCB range is actually 3528–3589
         return 'JCB';
     } else if ((card[0] === 3 && card[1] === 0 && card[2] === 0) || // Diners Club – Carte Blanche begins with 300, 301, 302, 303, 304, 305
@@ -256,7 +102,7 @@ const arrayFromString = string => {
     return numberArr;
 }
 
-// function to turn invalid cards to valid. By adding a number, which makes the sumCheck % 10 = 0. 
+// function to turn invalid card to valid. By adding a number, which makes the sumCheck % 10 = 0. 
 
 const turnInvalidCardValid = cardArr => {
     if (validateCred(cardArr) === false) {
@@ -291,6 +137,7 @@ const turnInvalidCardValid = cardArr => {
     }
 }
 
+
 // testing cards from this site: https://www.freeformatter.com/credit-card-number-generator-validator.html
 
 //JCB: begins with 3528-3589
@@ -299,16 +146,24 @@ const jcb2 = [3, 5, 2, 8, 1, 0, 0, 6, 5, 7, 1, 5, 0, 4, 1, 8]
 const jcb3 = [3, 5, 3, 9, 6, 7, 2, 4, 0, 4, 5, 0, 3, 6, 1, 8, 4, 8, 5]
 const jcb4 = [3, 5, 5, 4]; 
 for (let i=0; i < 14 ; i++) { // generating random card numbers
-    jcb4.push(Math.floor(Math.random() * 10))
+    jcb4.push(Math.floor(Math.random() * 10));
 };
 
 // Diners Club - North America: begins with 54
 const dcNa1 = [5, 4, 4, 6, 6, 2, 8, 0, 5, 8, 7, 7, 8, 7, 5, 2]
 const dcNa2 = [5, 4, 9, 7, 5, 3, 3, 5, 0, 7, 2, 4, 3, 7, 1]
+const dcNa3 = [5, 4]
+for (let i=0; i < 13; i++) {
+    dcNa3.push(Math.floor(Math.random() * 10));
+}
 // Diners Club - Carte Blanche: begins with 300-305
 const dcCb1 = [3, 0, 3, 1, 9, 2, 7, 9, 4, 8, 8, 0, 0, 9]
 const dcCb2 = [3, 0, 2, 2, 8, 5, 1, 8, 2, 1, 3, 6, 4, 1]
 const dcCb3 = [3, 0, 3, 7, 5, 0, 0, 8, 9, 2, 6, 2, 6, 8]
+const dcCb4 = [3, 0, 4]
+for (let i=0; i < 13; i++) {
+    dcCb4.push(Math.floor(Math.random() * 9));
+}
 // Diners Club - International: begins with 36
 const dcI1 = [3, 6, 8, 9, 6, 4, 8, 9, 9, 4, 7, 9, 9, 3]
 const dcI2 = [3, 6, 3, 9, 3, 0, 9, 6, 2, 5, 7, 4, 4, 6]
@@ -338,8 +193,8 @@ const masterC2 = [5, 4, 8, 4, 9, 3, 9, 4, 2, 3, 2, 8, 2, 2, 2, 0]
 
 const newBatch = [
     jcb1, jcb2, jcb3, jcb4,
-    dcNa1, dcNa2, 
-    dcCb1, dcCb2, dcCb3,
+    dcNa1, dcNa2, dcNa3,
+    dcCb1, dcCb2, dcCb3, dcCb4,
     dcI1, dcI2, dcI3,
     mae1, mae2, mae3,
     visaEl1, visaEl2, visaEl3,
@@ -353,34 +208,119 @@ const newBatch = [
 
 document.getElementById('try-card').innerHTML = 'Step 1. Generate a new card number to try.';
 let tryCardElement = document.getElementById('try-card');
-tryCardElement.style.color = 'red';
+tryCardElement.style.color = 'black';
 
 // generate a card number button interactivity:
 
-let generateCardBox = document.createElement('button');
-generateCardBox.id = 'cardBox';
-generateCardBox.innerHTML = 'Generate it!';
-document.getElementById('generate-a-new').appendChild(generateCardBox);
-generateCardBox.style.marginBottom = '2rem';
-generateCardBox.style.backgroundColor = 'red';
+let generateCardButton = document.createElement('button');
+generateCardButton.id = 'cardBox';
+generateCardButton.innerHTML = 'Generate a card number';
+document.getElementById('generate-a-new').appendChild(generateCardButton);
+generateCardButton.style.marginBottom = '2rem';
+generateCardButton.style.backgroundColor = 'red';
+
+
+let validateCardButton = document.createElement('button');
+validateCardButton.id = 'validateButton';
+validateCardButton.innerHTML = 'Validate the card';
+validateCardButton.style.backgroundColor = 'blue';
+validateCardButton.style.color = 'white';
+
+let turnInvalidValidButton = document.createElement('button');
+turnInvalidValidButton.id = 'turnInvalidValidButton';
+turnInvalidValidButton.innerHTML = 'Turn the card valid';
+turnInvalidValidButton.style.backgroundColor = 'blue';
+turnInvalidValidButton.style.color = 'white';
+
 
 function generateNewCard() {
-    let newNo = document.createElement('p');
+    function deletePreviousCard() { // clearing out the previous message;
+        let previousCard = document.getElementById('cardMessage');
+        if (previousCard) {
+            previousCard.remove();
+        }
+        let validateDiv = document.getElementById('trueMessage');
+        if (validateDiv) {
+            validateDiv.remove();
+        }
+        let turnInvalidValidDiv = document.getElementById('fixed-card');
+        if (turnInvalidValidDiv) {
+            turnInvalidValidDiv.remove();
+        }
+    };
+    deletePreviousCard(); 
     let randomCard = newBatch[Math.floor(Math.random() * newBatch.length)];
+    let newNo = document.createElement('p');
     let cardCo = getCardCompany(randomCard);
-    let cardMessage = randomCard.join('') + " (" + cardCo + ")";
+    let cardMessage = "Your card number is: <br><br>" + randomCard.join('') + " | " + cardCo;
     newNo.innerHTML = cardMessage;
+    newNo.id = 'cardMessage';
     document.getElementById('generate-a-new').appendChild(newNo);
-    generateCardBox.removeEventListener('click', generateNewCard); // this makes sure button is clickable only once
-    generateCardBox.style.backgroundColor = 'gray';
+    generateCardButton.innerHTML = 'Generate again';
+    generateCardButton.style.backgroundColor = 'blue';
+    generateCardButton.style.color = 'white';
 
+    // validating the card:
+    document.getElementById('validate-card').innerHTML = 'Step 2. Validate the card number.';
+    document.getElementById('validate-div').appendChild(validateCardButton);
+
+    function validateCard() {
+        function deletePreviousValidation() { // clearing out the previous message;
+            let validateDiv = document.getElementById('trueMessage');
+            if (validateDiv) {
+                validateDiv.remove();
+            }
+            let turnInvalidValidDiv = document.getElementById('fixed-card');
+            if (turnInvalidValidDiv) {
+            turnInvalidValidDiv.remove();
+            }
+        };
+        deletePreviousValidation(); 
+        let trueMessage = document.createElement('p');
+        trueMessage.id = 'trueMessage';
+        trueMessage.style.marginTop = '2rem';
+        trueMessage.style.fontWeight = 'bold';
+        if (validateCred(randomCard) === true) {
+            trueMessage.innerHTML = 'Your card is valid.';
+            trueMessage.style.color = 'green';
+        } else {
+            trueMessage.innerHTML = 'Your card is invalid.';
+            trueMessage.style.color = 'red';
+            document.getElementById('turn-valid-heading').innerHTML = '(Optional: Step 3.) Turn your invalid card valid.';
+            document.getElementById('turn-valid').appendChild(turnInvalidValidButton);
+
+            // generating a new valid number:
+            function turnValidFunction() {
+                function deletePreviousInvalidToValid() { // clearing out the previous message;
+                    let fixedDiv = document.getElementById('fixed-card');
+                    if (fixedDiv) {
+                        fixedDiv.remove();
+                    }
+                };
+                deletePreviousInvalidToValid(); 
+                correctCard = turnInvalidCardValid(randomCard);
+                let fixedCard = document.createElement('p');
+                let newMessage = "Your new card number is: <br><br>" + correctCard.join('') + " | " + cardCo;
+                fixedCard.innerHTML = newMessage;
+                fixedCard.id = 'fixed-card';
+                fixedCard.style.marginTop = '2rem';
+                document.getElementById('turn-valid').appendChild(fixedCard);
+            }
+
+            turnInvalidValidButton.addEventListener('click', turnValidFunction);
+        }
+        document.getElementById('validate-div').appendChild(trueMessage);
+    }
+    validateCardButton.addEventListener('click', validateCard);
+
+    // turning an invalid card calid: 
+    /*if (validateCred(randomCard) === false) {
+    }*/
 }
 
-generateCardBox.addEventListener('click', generateNewCard);
+generateCardButton.addEventListener('click', generateNewCard);
 
-let reset = function() {
 
-}
 
 /* order of creating a website flow: 
 
@@ -464,15 +404,8 @@ Flow B: user chooses to generate a credit card number of a certain company
 
 Click on a particular card company logo: 
 
-Generates a card number: 
-
-Click on a button to validate the card.
-
-Turns the card string to array. 
-
-Validates the card. 
-
-Tells the user.
+Done: Generates a card number: 
+Done: Click on a button to validate the card. Turns the card string to array. Validates the card. Tells the user.
 
 If invalid, click on a button to turn the card valid. 
 
